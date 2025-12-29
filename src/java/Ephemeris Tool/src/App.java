b8906fa5dad700d2d69ce339e9a7fcba1b8395fa
Tim Ashton <tim.ashton.la@gmail.com>
2025-12-28 19:09:49 -0800

    Stand-up Java implementation and move source files

---
+++ src/java/Ephemeris Tool/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ephemeris Tool v0.0");
+
+        NetworkUtils netUtilsObj = new NetworkUtils();
+        DataProcessor dataProcessorObj = new DataProcessor();
+        SourceManager sourceManagerObj = new SourceManager();
+    }
+}
---
+++ src/java/Ephemeris Tool/src/NetworkUtils.java
@@ -0,0 +1,5 @@
+public class NetworkUtils {
+    public void alive() {
+        System.out.println("Alive from NetworkUtils");
+    }
+}
